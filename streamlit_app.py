876473aa62603383e3eadfb0c3197ebd3f1aa129
Shubhamkhulbe <shubhamkhulbe79@gmail.com>
2023-06-22 15:50:48 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit;
 import pandas;
+import requests;
+import snowflake.connector;
 
 streamlit.title("My Parents New Healthy Diner");
 
@@ -25,7 +27,6 @@ streamlit.dataframe(fruits_to_show)
 
 #API response
 streamlit.header("Fruityvice Fruit Advice!")
-import requests
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
@@ -34,3 +35,6 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_ch
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Presenting data in tabular form 
 streamlit.dataframe(fruityvice_normalized)
+
+
+
